94763cb3c30cd19bfd5a13c0fc99a14a63479144
sritamcodes <sritamk3606@gmail.com>
2026-08-13 11:27:16 +0530

    Functions definitions

--- product.py
+++ product.py
@@ -1,6 +1,16 @@
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("++++++++++++++++++Supermarket ++++++++++++++++++++++")
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+def display():
+    print("Displaying")
+def search():
+    print("Searching")
+def add():
+    print("Adding")
+def update():
+    print("Updating")
+def delete():
+    print("Deleting")
 def main():
     while True:
         print("\n1. Display")
@@ -12,19 +22,19 @@ def main():
         choice = int(input("Enter your choice: "))
         match choice:
             case 1:
-                print("User Wants to Display the Product")
+                display()
             case 2:
-                print("User Wants to Search the Product")
+                search()
             case 3:
-                print("User Wants to Add a Product")
+                add()
             case 4:
-                print("User Wants to Update")
+                update()
             case 5:
-                print("User Wants to Delete")
+                delete()
             case 6:
                 print("Exiting...")
                 break
             case _:
                 print("Invalid Choice")
-if __name__=="__main":
+if __name__=="__main__":
     main()
